3b5004ffeda5a3d5bd5cb3dac4f71c25dd85d1f4
Jawad Alkassim <48631694+jawadalkassim@users.noreply.github.com>
2024-02-21 17:45:39 +0300

    add jornaya

--- v4/english.php
+++ v4/english.php
@@ -400,7 +400,11 @@ window._loq =[]
                                 <input type="hidden" id="local_storage" name="local_storage" value="">
                                 <input type="hidden" id="session_storage" name="session_storage" value="">
                                 <input type="hidden" name="xxTrustedFormCertUrl" id="xxTrustedFormCertUrl_0" value="https://cert.trustedform.com/454a35b802f3e7b63ffabb4efedb7c6ebe67886c">
+                                
                                 <input type="hidden" name="xxTrustedFormPingUrl" id="xxTrustedFormPingUrl_0" value="https://ping.trustedform.com/0.HJDqajg8vVF20hwiDBJ92kct5idepgBM4vHylmh43kShfWSZKwm8HI37KqPToi0y3dhTIQ3N.uOjUbzs7aKNUWivkGh7I9Q.50hgmcLFsDabNx1wtZZPdQ">
+<label><input type="hidden" id="leadid_tcpa_disclosure"/>[ENTER DESIRED OR SPECIFIED DISCLOSURE TEXT HERE]</label>
+                                <input id="leadid_token" name="universal_leadid" type="hidden" value=""/>
+
                                 <fieldset id="form-step1" class="form-steps fieldset-0" data-step="1" style="display: block;" data-tag="homeowner">
                                     <legend hidden="true">homeowner</legend>
                                     <h3 class="form_box-question">Are you a homeowner?</h3>
@@ -17085,6 +17089,20 @@ postCall("http://receiver.ceeleads.info/leads/ping", data).then(x => x.json()).t
         })(window, document, 'script', 'dataLayer', 'GTM-5SLK4FV');
     </script>
     <!-- End Google Tag Manager -->
+
+
+<script id="LeadiDscript" type="text/javascript">
+(function() {
+var s = document.createElement('script');
+s.id = 'LeadiDscript_campaign';
+s.type = 'text/javascript';
+s.async = true;
+s.src = '//create.lidstatic.com/campaign/5374621e-589a-c13f-4050-4c5786b29246.js?snippet_version=2';
+var LeadiDscript = document.getElementById('LeadiDscript');
+LeadiDscript.parentNode.insertBefore(s, LeadiDscript);
+})();
+</script>
+<noscript><img src='//create.leadid.com/noscript.gif?lac=FE5A6B76-3190-215D-18AE-036F796A10E7&lck=5374621e-589a-c13f-4050-4c5786b29246&snippet_version=2' /></noscript>
 
 
     <script type="text/javascript">
